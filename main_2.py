c35162b7fd5f1cd172107a0cf0cb07242c717186
Juan Burgos <burgos.135@gmail.com>
2022-12-09 15:39:32 +0100

    Imported the first training part, still not validated

--- main_2.py
+++ main_2.py
@@ -79,7 +79,7 @@ def main():
     # Data processing
     dataset, waveIndexBegin, waveIndexEnding = dataLoader.processData() 
     X_train, X_test, y_train, y_test = getWindowedSplitData(dataset, waveIndexBegin, waveIndexEnding, tStepLeftShift=-5, tStepRightShift=15, expectedWavesSizes=windowSizes)
-    X_train_ss, X_test_ss = MinMaxNormalization(X_train, X_test)             # Rescaling
+    X_train_ss, X_test_ss, mm = MinMaxNormalization(X_train, X_test)             # Rescaling
     
     print("X_train shape: ", X_train_ss.shape, "X_test_shape", X_test_ss.shape)
 
@@ -97,20 +97,97 @@ def main():
     config['nb_channels'] = X_train_ss.shape[2]
 
     #Calling the model
-    net = DeepConvLSTM(config=config)
+    
+    net = DeepConvLSTM_Simplified(config=config)
 
     loss = torch.nn.CrossEntropyLoss()
     opt = torch.optim.Adam(net.parameters(), lr=config['lr'], weight_decay=config["weight_decay"])
+    
     # Prepare for train
-    train_valid_net,_, val_output, train_output = train_simplified(X_train_ss, y_train, X_test_ss, y_test,
-        network=net, optimizer=opt, loss=loss, config=config, log_date=log_date,
-        log_timestamp=log_timestamp)
+    #net = train_simplified(X_train_ss, y_train, X_test_ss, y_test,
+    #    network=net, optimizer=opt, loss=loss, config=config, log_date=log_date,
+    #    log_timestamp=log_timestamp)
+    
+    #torch.save(net.state_dict(), './model1.pth')
+
+    # Quick validation
+
+    net.load_state_dict(torch.load('model1.pth'))
+    mySample = X_test_ss[2,:,:]
+    mySampleNew = mySample.reshape(mySample.shape[0], mySample.shape[1], 1)
+    myLabel = y_test[2]
+    mySample_Unnormalized = mm.inverse_transform(mySample)
+    print(mySample_Unnormalized.squeeze())
+    print(myLabel)
+    #prediction = net(mySampleNew)
+    #print(prediction)
+    #test1DLForHAR()
 
 
-def train_simplified(X_train_ss, y_train, X_test_ss, y_test,
-        network=net, optimizer=opt, loss=loss, config=config, log_date=log_date,
-        log_timestamp=log_timestamp):
-        pass
+
+def train_simplified(train_features, train_labels, val_features, val_labels,
+        network, optimizer, loss, config, log_date, log_timestamp):
+    """Use to train the network without using """
+    config['window_size'] = train_features.shape[1]
+    config['nb_channels'] = train_features.shape[2]
+
+    network.to(config['gpu'])
+    network.train()
+
+    train_dataset = torch.utils.data.TensorDataset(torch.from_numpy(train_features), torch.from_numpy(train_labels))
+    valid_dataset = torch.utils.data.TensorDataset(torch.from_numpy(val_features), torch.from_numpy(val_labels))
+    
+    trainLoader = DataLoader(train_dataset, batch_size = config['batch_size'], shuffle=True)
+    valLoader = DataLoader(valid_dataset, batch_size = config['batch_size'], shuffle=True)
+    
+    # print("Size ", len(trainLoader))
+    # for x,y in trainLoader:
+    #     print("Shape X", x.shape)
+    #     print("Shape y", y.shape)
+    #     break
+
+    optimizer, criterion = optimizer, loss
+
+    for e in range(config['epochs']):
+        train_losses = []
+        train_preds = []
+        train_gt = []
+        start_time = time.time()
+        batch_num = 1
+
+        for i, (x,y) in enumerate(trainLoader):
+            inputs, targets = x.to(config['gpu']), y.to(config['gpu'])
+            optimizer.zero_grad()
+
+            #forward
+            train_output = network(inputs)
+
+            #Calculate loss
+            # loss = criterion(train_output, targets.long())
+            loss = criterion(train_output, targets)
+
+            #Backprop
+            loss.backward()
+            optimizer.step()
+
+            train_output = torch.nn.functional.softmax(train_output, dim=1)
+
+            train_losses.append(loss.item())
+
+            #create predictions and true labels
+            y_preds = np.argmax(train_output.cpu().detach().numpy(), axis=-1)
+            y_true = targets.cpu().numpy().flatten()
+            train_preds = np.concatenate((np.array(train_preds, int), np.array(y_preds, int)))
+            train_gt = np.concatenate((np.array(train_gt, int), np.array(y_true, int)))
+
+            if batch_num % 10 == 0 and batch_num > 0:
+                cur_loss = np.mean(train_losses)
+                elapsed = time.time() - start_time
+                print('| epoch {:3d} | {:5d} batches | ms/batch {:5.2f} | train loss {:5.2f}'.format(e, batch_num, elapsed * 1000 / config['batch_size'], cur_loss))
+                start_time = time.time()
+            batch_num += 1
+
+    return network
 
 class Dataloader():
     """Following class has as responsabilities to get the data coming from the dataset.
@@ -140,6 +217,8 @@ class Dataloader():
         
         #Asssingning names to columns
         processedDataset.columns = ['Satus', 'Data', 'Bottle', 'Diff']
+        
+        #Replacing the values in the bottle to start froom 0 to 6
         
         # General information
         processedDataset.info()
@@ -198,7 +277,7 @@ def MinMaxNormalization(X_train, X_test):
         the X_train dataset with shape [windows, size_window]
     param X_test: np array
         the X_test dataset with shape [windows, size_window]
-    returns: normalize X_train_norm and X_test_norm"""
+    returns: normalize X_train_norm and X_test_norm, also the mm object"""
     mm = MinMaxScaler()
     X_train_flatten = X_train.flatten().reshape(-1,1)
     X_test_flatten = X_test.flatten().reshape(-1,1)
@@ -210,7 +289,7 @@ def MinMaxNormalization(X_train, X_test):
     X_test_ss = mm.transform(X_test_flatten)
     X_test_ss = X_test_ss.reshape(len(X_test), -1)
 
-    return X_train_ss, X_test_ss
+    return X_train_ss, X_test_ss, mm
 
 class DeepConvLSTM_Simplified(nn.Module):
     def __init__(self, config):
